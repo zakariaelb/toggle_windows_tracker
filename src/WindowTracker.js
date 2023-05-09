53895caa25eb919dd63c542a806b1aecb482cfe2
zakariaelb <zakari_a@icloud.com>
2023-05-09 22:27:44 +0400

    CleaningUP FUNCTION

--- src/WindowTracker.js
+++ src/WindowTracker.js
@@ -2,12 +2,34 @@ import React from "react"
 export default function WindowTracker() {
     
 
+// const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+    
+// React.useEffect(() => {
+//     window.addEventListener("resize", function() {
+//         setWindowWidth(window.innerWidth)
+//     })
+// }, [])
+
+// return (
+//     <h1>Window width: {windowWidth}</h1>
+// )
+// }
+
+// Cleanup function useEffect
 const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
     
 React.useEffect(() => {
-    window.addEventListener("resize", function() {
+    function watchWidth() {
+        console.log("Setting up...")
         setWindowWidth(window.innerWidth)
-    })
+    }
+    
+    window.addEventListener("resize", watchWidth)
+    
+    return function() {
+        console.log("Cleaning up...")
+        window.removeEventListener("resize", watchWidth)
+    }
 }, [])
 
 return (
